cd3d8055a5c57fb1b05761cd4bc499dbb5329c7e
Jerry Nixon <jerry.nixon@microsoft.com>
2024-12-03 12:55:55 -0700

    Added SWA support

--- start-data-api-builder/src/extension.ts
+++ start-data-api-builder/src/extension.ts
@@ -3,17 +3,19 @@ import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
   const startDabCommand = vscode.commands.registerCommand('dabExtension.startDab', async (uri: vscode.Uri) => {
-    if (uri && uri.fsPath.endsWith('dab-config.json')) {
+    if (uri && (uri.fsPath.endsWith('dab-config.json') || uri.fsPath.endsWith('staticwebapp.database.config.json'))) {
       const terminal = vscode.window.createTerminal('Data API');
       terminal.show();
       terminal.sendText(`cd "${path.dirname(uri.fsPath)}"`);
       terminal.sendText('dab start');
     } else {
-      vscode.window.showErrorMessage('This action can only be performed on a file named "dab-config.json".');
+      vscode.window.showErrorMessage(
+        'This action can only be performed on files named "dab-config.json" or "staticwebapp.database.config.json".'
+      );
     }
   });
 
   context.subscriptions.push(startDabCommand);
 }
 
-export function deactivate() {}
+export function deactivate() { }
