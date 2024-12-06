a30c0cb29c1f98a7037874a40e9233ee82f0884c
Jerry Nixon <jerry.nixon@microsoft.com>
2024-12-06 10:24:35 -0700

    Updated vsix files

--- package.bat
+++ package.bat
@@ -1,11 +1,7 @@
 :: Create the 'out' directory if it doesn't exist
 @echo off
-
-if not exist "./out" mkdir "./out"
-cd ./out
-if not exist "./old" mkdir "./old"
-move /Y *.vsix ./old
-cd ..
+rd out /s /q
+md out
 
 cd ./init-data-api-builder
 ECHO STARTING ./init-data-api-builder
@@ -35,13 +31,4 @@ cd ./visualize-data-api-builder
 ECHO STARTING ./visualize-data-api-builder
 call vsce package
 move /Y *.vsix ../out
-cd ..
-
-:: For each .vsix file in the 'out' directory, check if it also exists in the 'old' directory
-cd ./out
-for %%F in (*.vsix) do (
-    if exist "%cd%\old\%%F" (
-        del /Q /F "%cd%\old\%%F"
-    ) 
-)
 cd ..
